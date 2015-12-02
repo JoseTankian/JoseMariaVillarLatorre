5b950e1fe2ae828230b77998ac5534b34bb51485
JoseTankian <jose.ma.16@hotmail.com>
2015-12-02 19:06:42 +0100

    Movimiento_Jugador[2º Tarea]

---
+++ JoseMariaVillarLatorre/Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoJugador : MonoBehaviour {
+
+	Rigidbody2D  rg;
+	public float velocidadjug=8f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rg = GetComponent<Rigidbody2D> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			rg.velocity = (new Vector2 (-(velocidadjug), rg.velocity.y));
+		}
+		
+		if(Input.GetKey(KeyCode.D))
+		{
+			rg.velocity = (new Vector2 ((velocidadjug),rg.velocity.y));
+		}
+
+		Vector2 velocidad = GetComponent<Rigidbody2D>().velocity;
+		Debug.DrawLine(transform.position, new Vector3(transform.position.x+ velocidad.x,
+		                                               transform.position.y + velocidad.y, transform.position.z));
+	}
+}
