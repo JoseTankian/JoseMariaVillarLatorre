564e0d09c82dd9088c7b530c9fb3683edac0746c
JoseTankian <jose.ma.16@hotmail.com>
2015-12-02 19:57:52 +0100

    Disparo[3º Tarea]

---
+++ JoseMariaVillarLatorre/Assets/Scripts/Bala.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bala : MonoBehaviour {
+
+	//Creamos una objeto llamado proyectil
+	public GameObject proyectil;
+	//Creamos un vector llamado fuerza con valor x 0 y y 1000
+	public Vector2 fuerza = new Vector2(0,1000);
+	Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//llamamos al rigidbody
+		rg = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			//Creamos el proyectil y lo metemos en la variable nuevo_proyectil
+			GameObject nuevo_proyectil =(GameObject) Instantiate(proyectil, transform.position, transform.rotation);
+			//Extraemos en rigidbody para poder aplicarle una fuerza
+			rg = nuevo_proyectil.GetComponent<Rigidbody2D>();
+			//A los dos segundos se destruye el proyectil que ha sido invocado
+			Destroy(nuevo_proyectil, 2);
+			//Aplicamos la fuerza con el vector que hemos creado arriba
+			rg.AddForce(fuerza);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D objeto)
+	{
+		//Si se encuentra un objeto con el tag enemigo la bala se destruira
+		if(objeto.transform.tag == "enemigo")
+		{  
+			Destroy(gameObject);
+		}
+	}
+
+}
--- JoseMariaVillarLatorre/Assets/Scripts/MovimientoJugador.cs
+++ JoseMariaVillarLatorre/Assets/Scripts/MovimientoJugador.cs
@@ -4,29 +4,44 @@ using System.Collections;
 public class MovimientoJugador : MonoBehaviour {
 
 	Rigidbody2D  rg;
+	//Le asignamos velocidadjugador 8
 	public float velocidadjug=8f;
+
 
 	// Use this for initialization
 	void Start () 
 	{
+		//llamamos al rigidbody
 		rg = GetComponent<Rigidbody2D> (); 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		//Si pulsamos A le aplicamos al rigidbody al que se apolica una velicdad, 
+		//en el eje x la velocidadjug que es 8, y ninguna al eje y
 		if(Input.GetKey(KeyCode.A))
 		{
+			//ponemos menos para que sea -8
 			rg.velocity = (new Vector2 (-(velocidadjug), rg.velocity.y));
 		}
-		
+		//Lo mismo que hemos puesto arriba
 		if(Input.GetKey(KeyCode.D))
 		{
 			rg.velocity = (new Vector2 ((velocidadjug),rg.velocity.y));
 		}
 
-		Vector2 velocidad = GetComponent<Rigidbody2D>().velocity;
-		Debug.DrawLine(transform.position, new Vector3(transform.position.x+ velocidad.x,
-		                                               transform.position.y + velocidad.y, transform.position.z));
+
 	}
+
+
+
+
+
+
+
+
+
+
+
 }
